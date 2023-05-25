a40e4ae7bffd3d0a1bc9ef6397bb1ba0f2a5ac11
nerdError <55310192+nerdError@users.noreply.github.com>
2023-05-25 21:29:12 +0300

    'v23.0.0'

--- dist/tween.amd.js
+++ dist/tween.amd.js
@@ -817,7 +817,7 @@ define(['exports'], (function (exports) { 'use strict';
         return Tween;
     }());
 
-    var VERSION = '22.0.0';
+    var VERSION = '23.0.0';
 
     /**
      * Tween.js - Licensed under the MIT license
--- dist/tween.cjs.js
+++ dist/tween.cjs.js
@@ -819,7 +819,7 @@ var Tween = /** @class */ (function () {
     return Tween;
 }());
 
-var VERSION = '22.0.0';
+var VERSION = '23.0.0';
 
 /**
  * Tween.js - Licensed under the MIT license
